e45b82439296f09dca05b67f0c26dbeb457a5cbd
Kuriakose <kris.kuruvilla@gmail.com>
2024-07-29 02:50:40 +0530

    Added feature to retrieve user details successfully.

--- app/user/urls.py
+++ app/user/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("create/user/", views.UserCreateView.as_view(), name="create-user"),
+    path("list/user/", views.UserListView.as_view(), name="list-user"),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -27,3 +27,33 @@ class UserCreateView(APIView):
             )
             return Response({"message": "User created successfully."}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@extend_schema(
+parameters=[
+    OpenApiParameter(name='email', type=OpenApiTypes.EMAIL, description='Email of the user.', required=False,),
+    OpenApiParameter(name='mobile', type=OpenApiTypes.STR, description='Mobile of the user.', required=False,),
+],
+responses={status.HTTP_200_OK: serializers.UserSerializer()})
+class UserListView(APIView):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        email = request.query_params.get('email', None)
+        mobile = request.query_params.get('mobile', None)
+
+        if email:
+            user = models.User.objects.filter(email=email).first()
+        elif mobile:
+            user = models.User.objects.filter(mobile=mobile).first()
+        elif email and mobile:
+            user = models.User.objects.filter(email=email, mobile=mobile).first()
+        else:
+            return Response({"error": "Email or mobile number must be provided."}, status=status.HTTP_400_BAD_REQUEST)
+
+        if user:
+            serializer = serializers.UserSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "User not found."}, status=status.HTTP_404_NOT_FOUND)
